0236e1dbd7ca932a81ef386bb7599f9aa1879912
Alex McLeod <alex.mcleod@myagi.com>
2018-10-03 14:31:16 +0100

    Hardcoded hasLoopSupport value

--- src/components/indicator/index.js
+++ src/components/indicator/index.js
@@ -1,10 +1,9 @@
 import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
 import { Animated, Easing } from 'react-native';
-import RN from 'react-native/package';
 
-const [major, minor] = RN.version.split('.').map((item) => Number(item));
-const hasLoopSupport = !major && minor >= 45;
+// Hardcoded this to ensure react-native-web support.
+const hasLoopSupport = true;
 
 export default class Indicator extends PureComponent {
   static defaultProps = {
